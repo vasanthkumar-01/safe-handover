5c1b6514ab1dc75aae0c78f66b05cfa83c52b994
vasanthkumar-01 <solaivasanthakumar192006@gmail.com.com>
2026-09-04 12:52:06 +0530

    Connect frontend to deployed backend

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://safe-handover.onrender.com',
   headers: { 'Content-Type': 'application/json' },
 })
 
